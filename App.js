c47da3bc821770dd27c99eb86a5f317ac6053566
filipestrike <filipe.nogueira2001@gmail.com>
2023-08-30 20:31:17 -0300

    commit inicial projeto

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+} from "react-native";
+import logo from "./assets/g aroto.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.formTitle}>Tagarela</Text>
+      <Text style={styles.formLogin}>Login</Text>
+      <TextInput
+        style={styles.formInput}
+        placeholder="Usuário"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoComplete="email"
+      ></TextInput>
+      <TextInput
+        style={styles.formInput}
+        placeholder="Senha"
+        autoCapitalize="none"
+        secureTextEntry
+      ></TextInput>
+      <Pressable style={styles.formButton}>
+        <Text style={styles.formTextButton}>Continuar</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +39,53 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#AAC8DA",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    margin: 10,
+  },
+  formTitle: {
+    color: "#004F84",
+    alignItems: "center",
+    font: "Mitr",
+    fontSize: 64,
+    marginBottom: 20,
+  },
+  formLogin: {
+    color: "#004F84",
+    alignItems: "center",
+    font: "Mitr",
+    fontSize: 48,
+    marginTop: 48,
+    marginBottom: 28,
+  },
+  formInput: {
+    borderRadius: 30,
+    borderWidth: 1,
+    borderRadius: 30,
+    fontSize: 25,
+    width: "80%",
+    backgroundColor: "#D9D9D9",
+    padding: 10,
+    margin: 10,
+  },
+  formButton: {
+    backgroundColor: "#004F84",
+    borderWidth: 1,
+    borderRadius: 20,
+    width: "35%",
+    padding: 10,
+    margin: 20,
+  },
+  formTextButton: {
+    color: "#FFF",
+    alignItems: "center",
+    fontSize: 24,
+    fontFamily: "lexend",
+    width: "100%",
   },
 });
