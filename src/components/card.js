dbe8f31a236e67b362526e61497f39649fb84c75
filipestrike <filipe.nogueira2001@gmail.com>
2023-11-08 13:46:54 -0300

    Ajuste de conflto de versao dos  expo-font@11.6.0 - expected version: ~11.4.0   expo-splash-screen@0.22.0 - expected version: ~0.20.5   expo-status-bar@1.7.1 - expected version: ~1.6.0   react-native@0.72.5 - expected version: 0.72.6   react-native-gesture-handler@2.13.1 - expected version: ~2.12.0   react-native-reanimated@3.5.4 - expected version: ~3.3.0   react-native-screens@3.25.0 - expected version: ~3.22.0   react-native-svg@13.14.0 - expected version: 13.9.0

--- src/components/card.js
+++ src/components/card.js
@@ -1,52 +1,58 @@
 import {
-    StyleSheet,
-    TouchableOpacity,
-    Dimensions,
-    View,
-    Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  View,
+  Text,
 } from "react-native";
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 const cardWidth = width * 0.2; // Card width based on screen size
 const cardHeight = height * 0.35; // Card height based on screen size
 const cardMargin = width * 0.005; // Card margin based on screen size
 
 const CardModel = () => {
-    return(
-        <View style={styles.container}>
-            <TouchableOpacity style={[styles.cardButton, { width: cardWidth, height: cardHeight, marginVertical: cardMargin }]} onPress={() => {
-                // Aqui vai a logica para executar o sonido da card
-                alert('Card Pressed!');
-            }}>
-                {/* Aqui vai a logica para trazer a imagen do banco de dados */}
-            </TouchableOpacity>
-            <Text style={styles.cardText}>
-                {/* aqui vai a logica para trazer o text descrição da imagen */}
-                CARD TEXT
-            </Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={[
+          styles.cardButton,
+          { width: cardWidth, height: cardHeight, marginVertical: cardMargin },
+        ]}
+        onPress={() => {
+          // Aqui vai a logica para executar o sonido da card
+          alert("Card Pressed!");
+        }}
+      >
+        {/* Aqui vai a logica para trazer a imagen do banco de dados */}
+      </TouchableOpacity>
+      <Text style={styles.cardText}>
+        {/* aqui vai a logica para trazer o text descrição da imagen */}
+        CARD TEXT
+      </Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#FFFFFF",
-    },
-    cardButton: {
-        backgroundColor: "#F4F4F4",
-        borderRadius: 30,
-        marginTop: 20,
-        marginHorizontal: cardMargin,
-        // Width and height set dynamically
-    },
-    cardText: {
-        marginTop: 5,
-        fontFamily: "Mitr_500Medium",
-        fontSize: width * 0.02, // Font size based on screen size
-        color: "#000000",
-    },
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  cardButton: {
+    backgroundColor: "#F4F4F4",
+    borderRadius: 30,
+    marginTop: 20,
+    marginHorizontal: cardMargin,
+    // Width and height set dynamically
+  },
+  cardText: {
+    marginTop: 5,
+    fontFamily: "Mitr_500Medium",
+    fontSize: width * 0.02, // Font size based on screen size
+    color: "#000000",
+  },
 });
 
 export default CardModel;
--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,8 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import {
-    StyleSheet,
-    View,
-} from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import CardsCategories from "../components/cardsBar";
 import OptionsMenu from "../components/optionsBar";
@@ -10,31 +7,37 @@ import CardModel from "../components/card";
 import HeaderComponent from "../components/header";
 
 const Home = () => {
-    return(
-        <View style={styles.containerFather}>
-            <HeaderComponent />
-            <View style={styles.containerBars}>
-                <CardsCategories />
-            </View>
-            <CardModel />
-            <View style={styles.containerBars}>
-                <OptionsMenu homeColor="#5E5CB2" favColor="#949494" addColor="#949494" categoriesColor="#949494" userColor="#949494" />
-            </View>
-            <StatusBar />
-        </View>
-    );
+  return (
+    <View style={styles.containerFather}>
+      <HeaderComponent />
+      <View style={styles.containerBars}>
+        <CardsCategories />
+      </View>
+      <CardModel />
+      <View style={styles.containerBars}>
+        <OptionsMenu
+          homeColor="#5E5CB2"
+          favColor="#949494"
+          addColor="#949494"
+          categoriesColor="#949494"
+          userColor="#949494"
+        />
+      </View>
+      <StatusBar />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    containerFather: {
-        backgroundColor: "#FFFFFF",
-        flex: 1,
-    },
-    containerBars: {
-        width: "100%",
-        justifyContent: "center",
-        marginVertical: 30,
-    },
+  containerFather: {
+    backgroundColor: "#FFFFFF",
+    flex: 1,
+  },
+  containerBars: {
+    width: "100%",
+    justifyContent: "center",
+    marginVertical: 30,
+  },
 });
 
 export default Home;
