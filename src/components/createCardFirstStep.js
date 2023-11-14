74e03c024a0e7294aff68e6f09af491373a2e734
Lucca Valadares <luccavaladares01@gmail.com>
2023-11-14 20:54:28 -0300

    upload image and audio

--- src/components/createCardFirstStep.js
+++ src/components/createCardFirstStep.js
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
-  Dimensions,
-  TextInput,
   View,
   TouchableOpacity,
   Text,
+  TextInput,
+  Image,
+  Alert,
 } from "react-native";
 import Svg, { Path } from "react-native-svg";
 import * as ImagePicker from "expo-image-picker";
-import { useState, useEffect } from "react";
-import { Image } from "react-native";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import storage from "./fireBaseConfig"
-import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import storage from "./fireBaseConfig";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import iconCheck from "/src/common/assets/icons/icon_check.svg";
 
-const { width, height } = Dimensions.get("window");
-const fontSize = Math.min(width, height) * 0.03; // Font size based on screen size
+const firestore = getFirestore();
 
 const StepOneCreate = () => {
-  const [image, setImage] = useState("");
+  const [nodeName, setNodeName] = useState("");
+  const [image, setImage] = useState(null);
+  const [audio, setAudio] = useState(null);
+  const [showImageUpload, setShowImageUpload] = useState(true);
+  //const [showAudioUpload, setShowAudioUpload] = useState(true);
 
   const handleImagePicker = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -30,7 +33,7 @@ const StepOneCreate = () => {
       quality: 1,
     });
 
-    if (!result.canceled) {
+    if (!result.cancelled) {
       const imageUri = result.assets[0].uri;
       if (imageUri) {
         // Call the uploadImage function with the valid image URI
@@ -41,6 +44,7 @@ const StepOneCreate = () => {
     }
   };
 
+  // Função para escolher uma imagem
   const uploadImage = async (uri, fileType) => {
     const response = await fetch(uri);
     const blob = await response.blob();
@@ -69,33 +73,98 @@ const StepOneCreate = () => {
     );
   };
 
+  // Função para escolher um arquivo de áudio
+  const uploadAudio = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Audio,
+    });
+
+    if (!result.cancelled) {
+      setAudio(result.uri);
+    }
+  };
+
+  // Função para fazer o upload dos dados para o Firebase
+  const uploadDataToFirebase = async () => {
+    try {
+      // Upload da imagem para o Firebase Storage
+      const imageRef = storage.ref().child(`images/${nodeName}`);
+      const imageSnapshot = await imageRef.putFile(image);
+      const imageUrl = await imageSnapshot.ref.getDownloadURL();
+
+      // Upload do áudio para o Firebase Storage
+      const audioRef = storage.ref().child(`audio/${nodeName}`);
+      const audioSnapshot = await audioRef.putFile(audio);
+      const audioUrl = await audioSnapshot.ref.getDownloadURL();
+
+      // Salvar os dados no Firestore
+      await addDoc(collection(firestore, "seu_colecao"), {
+        nodeName,
+        imageUrl,
+        audioUrl,
+      });
+
+      Alert.alert("Sucesso", "Dados salvos com sucesso!");
+    } catch (error) {
+      console.error("Erro ao salvar os dados:", error);
+      Alert.alert(
+        "Erro",
+        "Ocorreu um erro ao salvar os dados. Tente novamente."
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
         style={styles.cardNameInput}
         placeholder="NOME DO CARD"
         placeholderTextColor="#5E5CB2"
+        value={nodeName}
+        onChangeText={(text) => setNodeName(text)}
       />
+      {showImageUpload && (
+        <TouchableOpacity
+          style={styles.uploadButton}
+          onPress={() => {
+            handleImagePicker();
+          }}
+        >
+          <Text style={styles.textUpload}>UPLOAD IMAGEM</Text>
+          <Svg
+            width="36"
+            height="33"
+            viewBox="0 0 36 33"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <Path
+              d="M0 0V4.03183H35.7206V0H0ZM17.8603 8.06367L4.46508 20.1592H13.3952V32.2547H22.3254V20.1592H31.2555L17.8603 8.06367Z"
+              fill="#5E5CB2"
+            />
+          </Svg>
+        </TouchableOpacity>
+      )}
+      {image && <View style={styles.iconCheckContainer}>{iconCheck}</View>}
       <TouchableOpacity
         style={styles.uploadButton}
         onPress={() => {
-          handleImagePicker(); // Chama a função handleImagePicker quando o botão é pressionado
+          // Call the function to upload audio
+          uploadAudio();
         }}
       >
-        <Text style={styles.textUpload}>UPLOAD IMAGEM</Text>
-        <Image source={{ uri: image }} />
-        <Svg
-          width="36"
-          height="33"
-          viewBox="0 0 36 33"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <Path
-            d="M0 0V4.03183H35.7206V0H0ZM17.8603 8.06367L4.46508 20.1592H13.3952V32.2547H22.3254V20.1592H31.2555L17.8603 8.06367Z"
-            fill="#5E5CB2"
-          />
-        </Svg>
+        <Text style={styles.textUpload}>UPLOAD ÁUDIO</Text>
+      </TouchableOpacity>
+      {/* Substituído trecho de código pelo ícone check */}
+      {audio && <View style={styles.iconCheckContainer}>{iconCheck}</View>}
+      <TouchableOpacity
+        style={styles.uploadButton}
+        onPress={() => {
+          // Call the function to upload all data to Firebase
+          uploadDataToFirebase();
+        }}
+      >
+        <Text style={styles.textUpload}>SALVAR DADOS</Text>
       </TouchableOpacity>
     </View>
   );
@@ -165,11 +234,17 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     fontFamily: "Mitr_400Regular",
     padding: 20,
-    fontSize: fontSize, // Responsive font size
+    fontSize: 16, // Responsive font size
     height: 66,
     width: "40%", // Adjust as needed
     borderColor: "#F4F4F4",
     backgroundColor: "#F4F4F4",
+  },
+
+  iconCheckContainer: {
+    position: "absolute",
+    top: 10,
+    right: 10,
   },
 });
 
