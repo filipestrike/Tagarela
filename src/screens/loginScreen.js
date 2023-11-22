899c2aa7239c2d122bf33ab0b989430f06fbd68c
filipestrike <filipe.nogueira2001@gmail.com>
2023-11-22 19:59:20 -0300

    ajuste css e headerlogin

--- src/screens/loginScreen.js
+++ src/screens/loginScreen.js
@@ -1,20 +1,32 @@
 import React, { useEffect } from "react";
-import { StatusBar, Dimensions, StyleSheet, TextInput, View, Text } from "react-native";
+import {
+  StatusBar,
+  Dimensions,
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+} from "react-native";
 import SubmitButton from "../components/submitButton";
 import CreateButton from "../components/createButton";
 import { useNavigation } from "@react-navigation/native";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { app } from '../components/fireBaseConfig';
-import { setPersistence, browserLocalPersistence } from 'firebase/auth'; // Importe setPersistence e browserLocalPersistence
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { app } from "../components/fireBaseConfig";
+import { setPersistence, browserLocalPersistence } from "firebase/auth"; // Importe setPersistence e browserLocalPersistence
+import HeaderLoginComponent from "../components/headerLogin";
 
 const { width, height } = Dimensions.get("window");
 const fontSize = Math.min(width, height) * 0.03;
 
 const FormLogin = () => {
   const navigation = useNavigation();
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [errorMessage, setErrorMessage] = React.useState('');
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
 
   const auth = getAuth(app);
 
@@ -22,9 +34,11 @@ const FormLogin = () => {
     async function configurePersistence() {
       try {
         await setPersistence(auth, browserLocalPersistence);
-        console.log('Persistência definida como AsyncStorage para sessão local');
+        console.log(
+          "Persistência definida como AsyncStorage para sessão local"
+        );
       } catch (error) {
-        console.error('Erro ao definir a persistência:', error);
+        console.error("Erro ao definir a persistência:", error);
       }
     }
 
@@ -33,39 +47,40 @@ const FormLogin = () => {
   const handleCreateAccount = () => {
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        console.log('Conta criada!');
+        console.log("Conta criada!");
         const user = userCredential.user;
         console.log(user);
-        alert('Conta criada com sucesso!');
+        alert("Conta criada com sucesso!");
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-        setErrorMessage('Erro ao criar conta: ' + error.message);
+        setErrorMessage("Erro ao criar conta: " + error.message);
       });
-  }
+  };
 
   const handleSignIn = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        console.log('Login bem-sucedido');
+        console.log("Login bem-sucedido");
         const user = userCredential.user;
         console.log(user);
-        navigation.navigate('Home');
+        navigation.navigate("Home");
       })
-      .catch(error => {
-        console.error('Erro durante o login:', error);
+      .catch((error) => {
+        console.error("Erro durante o login:", error);
         console.log(error);
-        setErrorMessage('CRIE UMA CONTA PARA CONTINUAR');
+        setErrorMessage("CRIE UMA CONTA PARA CONTINUAR");
       });
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <View>
+      <HeaderLoginComponent />
+      <View styles={styles.TopBar}>
         {/* TextInput para o email */}
         <TextInput
           style={styles.input}
-          onChangeText={text => setEmail(text)}
+          onChangeText={(text) => setEmail(text)}
           value={email}
           placeholder="E-mail"
           keyboardType="email-address"
@@ -77,7 +92,7 @@ const FormLogin = () => {
         {/* TextInput para a senha */}
         <TextInput
           style={styles.input}
-          onChangeText={text => setPassword(text)}
+          onChangeText={(text) => setPassword(text)}
           value={password}
           placeholder="Senha"
           secureTextEntry={true}
@@ -91,7 +106,9 @@ const FormLogin = () => {
       <StatusBar style="auto" />
 
       <View style={styles.errorContainer}>
-        {errorMessage !== '' && <Text style={styles.errorText}>{errorMessage}</Text>}
+        {errorMessage !== "" && (
+          <Text style={styles.errorText}>{errorMessage}</Text>
+        )}
       </View>
     </View>
   );
@@ -105,8 +122,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   input: {
-    height: "60%",
-    width: "100%",
+    width: 400,
     borderWidth: 3,
     borderRadius: 20,
     fontFamily: "Mitr_400Regular",
@@ -116,11 +132,9 @@ const styles = StyleSheet.create({
     padding: 15,
     margin: 10,
   },
-  errorContainer: {
-    
-  },
+  errorContainer: {},
   errorText: {
-    color: 'red',
+    color: "red",
     fontSize: 16,
   },
 });
