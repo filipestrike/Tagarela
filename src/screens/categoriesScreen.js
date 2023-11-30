f86f841ad677e10304f389410a305eb9ba3c77e7
Lucca Valadares <luccavaladares01@gmail.com>
2023-11-29 22:32:52 -0300

    save localmente datas create card

--- src/screens/categoriesScreen.js
+++ src/screens/categoriesScreen.js
@@ -1,41 +1,101 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import HeaderComponent from "../components/header";
 import CardsCategoriesEdit from "../components/cardsBarEdit";
-import OptionsMenu from "../components/optionsBar";
-import CardModelFav from "../components/CardModel";
+
+
+import { Audio } from "expo-av";
 
 const Categories = () => {
+  const [cardName, setCardName] = useState("");
+  const [imageUrl, setImageUrl] = useState(null);
+  const [audioUrl, setAudioUrl] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const name = await AsyncStorage.getItem("cardName");
+        const image = await AsyncStorage.getItem("savedImage");
+        const audio = await AsyncStorage.getItem("savedAudio");
+
+        if (name !== null) {
+          setCardName(name);
+        }
+
+        if (image !== null) {
+          setImageUrl(image);
+        }
+
+        if (audio !== null) {
+          setAudioUrl(audio);
+        }
+      } catch (error) {
+        console.error("Error retrieving data:", error);
+      }
+    };
+
+    getData();
+  }, []);
+
+  const playAudio = async () => {
+    if (audioUrl) {
+      const soundObject = new Audio.Sound();
+
+      try {
+        await soundObject.loadAsync({ uri: audioUrl });
+        await soundObject.playAsync();
+      } catch (error) {
+        console.error("Error playing audio:", error);
+      }
+    }
+  };
+
   return (
     <View style={styles.containerFather}>
-      <HeaderComponent />
-      <View style={styles.containerBars}>
-        <CardsCategoriesEdit />
+    <HeaderComponent />
+    <View style={styles.containerBars}>
+      <CardsCategoriesEdit />
       </View>
-      <CardModelFav />
-      <View style={styles.containerBars}>
-        <OptionsMenu
-          homeColor="#949494"
-          favColor="#949494"
-          addColor="#949494"
-          categoriesColor="#5E5CB2"
-          userColor="#949494"
-        />
+      <View style={styles.dataContainer}>
+        <Text style={styles.cardName}>{cardName}</Text>
+        {imageUrl && <Image source={{ uri: imageUrl }} style={styles.image} />}
+        {audioUrl && (
+          <TouchableOpacity onPress={playAudio}>
+            <Text style={styles.audioText}>Play Audio</Text>
+          </TouchableOpacity>
+        )}
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  containerFather: {
-    backgroundColor: "#FFFFFF",
+  container: {
     flex: 1,
-  },
-  containerBars: {
-    width: "100%",
+    backgroundColor: "#FFFFFF",
+    alignItems: "center",
     justifyContent: "center",
-    marginVertical: 30,
+  },
+  dataContainer: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  cardName: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "cover",
+    marginBottom: 10,
+  },
+  audioText: {
+    fontSize: 18,
+    color: "blue",
+    textDecorationLine: "underline",
   },
 });
 
--- src/screens/createCardScreen.js
+++ src/screens/createCardScreen.js
@@ -1,64 +1,179 @@
+import React, { useState } from "react";
 import {
-    StyleSheet,
-    View,
-    Text,
-    Dimensions
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Text,
+  Image,
+  Alert,
 } from "react-native";
-import Svg, { Path } from 'react-native-svg';
+import * as ImagePicker from "expo-image-picker";
+import * as DocumentPicker from "expo-document-picker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import iconCheck from "../common/assets/icons/icon_check.svg";
+import { useNavigation } from "@react-navigation/core";
 
-import OptionsMenu from "../components/optionsBar";
-import HeaderComponent from "../components/header";
-import CardModel from "../components/card";
-import StepOneCreate from "../components/createCardFirstStep";
+const StepOneCreate = () => {
+  const [nodeName, setNodeName] = useState("");
+  const [imageUrl, setImageUrl] = useState(null);
+  const [audioUrl, setAudioUrl] = useState(null);
+  const [showCheckIcon, setShowCheckIcon] = useState(false);
+  const [showCheckIconAudio, setShowCheckIconAudio] = useState(false);
+  const navigation = useNavigation();
 
-const { width, height } = Dimensions.get('window');
-const fontSize = Math.min(width, height) * 0.05; // Font size based on screen size
+  const handleImagePicker = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      aspect: [4, 4],
+      allowsEditing: true,
+      base64: true,
+      quality: 1,
+    });
 
-const CreateCard = () => {
-    return (
-        <View style={styles.containerFather}>
-            <HeaderComponent />
-            <View style={styles.containerStatement}>
-                <Svg width={70} height={60} viewBox="0 0 70 60" fill="#5E5CB2" xmlns="http://www.w3.org/2000/svg">
-                    <Path d="M18.3418 0V20.2168H0V33.6946H18.3418V53.9114H30.5696V33.6946H48.9114V20.2168H30.5696V0H18.3418Z" />
-                </Svg>
-                <Text style={styles.enunciatingText}>CRIANDO CARD</Text>
-            </View>
-            <View style={styles.containerContent}>
-                <StepOneCreate />
-            </View>
-            <View style={styles.containerBars}>
-                <OptionsMenu homeColor="#949494" favColor="#949494" addColor="#5E5CB2" categoriesColor="#949494" userColor="#949494" />
-            </View>
-        </View>
-    );
+    if (!result.cancelled) {
+      const imageUri = result.uri;
+      if (imageUri) {
+        try {
+          await AsyncStorage.setItem("savedImage", imageUri);
+          setImageUrl(imageUri);
+          setShowCheckIcon(true);
+        } catch (error) {
+          console.error("Error saving image locally:", error);
+        }
+      } else {
+        console.error("Invalid image URI");
+      }
+    }
+  };
+
+  const handleAudioPicker = async () => {
+    let result = await DocumentPicker.getDocumentAsync({
+      type: "audio/mpeg",
+      copyToCacheDirectory: false,
+    });
+
+    if (!result.cancelled) {
+      const { uri } = result;
+
+      try {
+        await AsyncStorage.setItem("savedAudio", uri);
+        setAudioUrl(uri);
+        setShowCheckIconAudio(true);
+      } catch (error) {
+        console.error("Error saving audio locally:", error);
+      }
+    }
+  };
+
+  const saveCardLocally = async () => {
+    try {
+      // Salvar os dados localmente (nome, imagem, áudio)
+      await AsyncStorage.setItem("cardName", nodeName);
+      // Adicione outras informações do card que deseja salvar localmente
+
+      navigation.navigate("Categories");
+    } catch (error) {
+      console.error("Error saving card locally:", error);
+    }
+  };
+
+  return (
+    <View style={styles.NameContainer}>
+      <TextInput
+        style={styles.cardNameInput}
+        placeholder="CARD NAME"
+        placeholderTextColor="#5E5CB2"
+        value={nodeName}
+        onChangeText={(text) => setNodeName(text)}
+      />
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={handleImagePicker}
+      >
+        <Text style={styles.textUpload}>UPLOAD IMAGE</Text>
+        {showCheckIcon && (
+          <View style={styles.iconCheckContainer}>
+            <Image source={iconCheck} style={styles.checkIcon} />
+          </View>
+        )}
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={handleAudioPicker}
+      >
+        <Text style={styles.textUpload}>UPLOAD AUDIO</Text>
+        {showCheckIconAudio && (
+          <View style={styles.iconCheckContainer}>
+            <Image source={iconCheck} style={styles.checkIcon} />
+          </View>
+        )}
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.confirmData} onPress={saveCardLocally}>
+        <Text style={styles.textConfirm}>CREATE CARD</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    containerFather: {
-        backgroundColor: "#FFFFFF",
-        flex: 1,
-    },
-    containerContent:{
-        width: "100%",
-        justifyContent: "center",
-    },
-    containerBars: {
-        width: "100%",
-        justifyContent: "center",
-        marginVertical: 30,
-    },
-    containerStatement: {
-        flexDirection: "row",
-        justifyContent: "center",
-        marginVertical: 20,
-    },
-    enunciatingText:{
-        fontFamily: "Mitr_500Medium",
-        fontSize: fontSize,
-        color: "#5E5CB2",
-        justifyContent: "center",
-    },
+  NameContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  cardNameInput: {
+    borderWidth: 1,
+    borderRadius: 30,
+    fontFamily: "Mitr_400Regular",
+    padding: 20,
+    fontSize: 16,
+    height: 66,
+    width: "40%",
+    borderColor: "#F4F4F4",
+    backgroundColor: "#F4F4F4",
+  },
+  buttonConteiner: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 15,
+    borderRadius: 30,
+    width: 225,
+    height: 191,
+    backgroundColor: "#F4F4F4",
+  },
+  textUpload: {
+    fontFamily: "Mitr_400Regular",
+    color: "#5E5CB2",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  iconCheckContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  checkIcon: {
+    position: "absolute",
+    marginTop: 20,
+    width: 40,
+    height: 30,
+  },
+  confirmData: {
+    width: 211,
+    height: 50,
+    border: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 15,
+    borderRadius: 30,
+    backgroundColor: "#5E5CB2",
+  },
+  textConfirm: {
+    fontFamily: "Mitr_400Regular",
+    color: "#FFFFFF",
+    fontSize: 20,
+    marginVertical: 10,
+  },
 });
 
-export default CreateCard;
+export default StepOneCreate;
